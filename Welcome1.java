942a7415932b43671105ec2dc1aef751702e2a34
avierusu <avierusu@gmail.com>
2023-03-26 14:10:20 -0500

    prints Hello, then each character

--- Welcome1.java
+++ Welcome1.java
@@ -1,7 +1,12 @@
 package chapter2.examples;
 
 public class Welcome1{
-    public static void main(String[] argssssssssss){
-        System.out.println("Welcome to Java yay");
+    public static void main(String[] args){
+        String text = "Hello";
+        System.out.println(text);
+
+        for (int index = 0; index <= text.length(); index++){
+            System.out.println(text.charAt(index));
+        }
     }
 }
